0c2910ea10a1f2a513e0e381652ce05192046d7f
eff-im <efim-bukin@yandex.ru>
2019-07-29 21:56:23 +0700

    Добавил определение первого числа факториала !N

--- python_1.py
+++ python_1.py
@@ -3,5 +3,8 @@ def squirrel(N):
     F = 1
     for i in range(1, N + 1):
         F *= i
-    return F
-
+    k = 1
+    for i in range (1, len(str(F))):
+        k *= 10
+    pervoe_chislo = F / k
+    return int(pervoe_chislo)
